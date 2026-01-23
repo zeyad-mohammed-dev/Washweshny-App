770cba9140ebd807d487870afa09c30653fbb19e
Zeyad Mohammed <zeyadmohammed200493@gmail.com>
2026-01-23 10:34:38 +0200

    feat(db): add user model

---
+++ src/DB/models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+export const genderEnum = { male: 'male', female: 'female' };
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: [2, 'First name must be at least 2 characters long'],
+      maxlength: [25, 'First name must be at most 25 characters long'],
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlength: [2, 'Last name must be at least 2 characters long'],
+      maxlength: [25, 'Last name must be at most 25 characters long'],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, 'Password must be at least 6 characters long'],
+    },
+    gender: {
+      type: String,
+      enum: Object.values(genderEnum),
+      default: genderEnum.male,
+    },
+    phone: String,
+    confirmEmail: Date,
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+userSchema
+  .virtual('fullName')
+  .get(function () {
+    return this.firstName + ' ' + this.lastName;
+  })
+  .set(function (value) {
+    const [firstName, lastName] = value?.split(' ') || [];
+    this.firstName = firstName;
+    this.lastName = lastName;
+  });
+
+export const UserModel =
+  mongoose.model.User || mongoose.model('User', userSchema);
+
+UserModel.syncIndexes();
